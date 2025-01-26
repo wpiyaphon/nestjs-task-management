aba80ce2cfeafe95965ce2771d1d0982ab3f599b
Piyaphon Wu (Hong) <piyaphon@greenmoons.co.th>
2025-01-26 16:35:12 +0700

    create task service and model

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -5,8 +5,8 @@ export interface Task {
   taskStatus: TaskStatus;
 }
 
-enum TaskStatus {
-  OPEN = 'OPEN',
+export enum TaskStatus {
+  TODO = 'TODO',
   IN_PROGRESS = 'IN_PROGRESS',
   DONE = 'DONE',
 }
