6dcf97b51132cb621c8503b24f347ade77542f22
Piyaphon Wu (Hong) <piyaphon@greenmoons.co.th>
2025-01-31 00:22:42 +0700

    add validator for get and update task services

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,9 +9,10 @@ import {
   Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
+import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -35,8 +36,9 @@ export class TasksController {
   @Patch(':id/status')
   updateTaskStatusById(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
   ) {
+    const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 
